efdfbfa0c9f9ca73165abf2b1713305a02ecd531
Morgan Prior <morgan_prior@uri.edu>
2023-05-29 16:11:50 -0400

    added levenshtein distance

--- src/distances/misc.rs
+++ src/distances/misc.rs
@@ -23,3 +23,46 @@ pub fn cosine<T: Number, U: Number>(x: &[T], y: &[T]) -> U {
 pub fn hamming<T: Number, U: Number>(x: &[T], y: &[T]) -> U {
     U::from(x.iter().zip(y.iter()).filter(|(&a, &b)| a != b).count()).unwrap()
 }
+
+
+pub fn levenshtein<T: Number, U: Number>(x: &[T], y: &[T]) -> U {
+    let (len_x, len_y) = (x.iter().count(), y.iter().count());
+
+    if len_x == 0 {
+        // handle special case of 0 length
+        U::from(len_x).unwrap()
+    } else if len_y == 0 {
+        // handle special case of 0 length
+        U::from(len_y).unwrap()
+    } else if len_x < len_y {
+        // require len_a < len_b
+        levenshtein(y, x)
+    } else {
+        let len_y = len_y + 1;
+
+        // initialize DP table for string y
+        let mut cur: Vec<usize> = (0..len_y).collect();
+
+        // calculate edit distance
+        for (i, cx) in x.iter().enumerate() {
+            // get first column for this row
+            let mut pre = cur[0];
+            cur[0] = i + 1;
+            for (j, cy) in y.iter().enumerate() {
+                let tmp = cur[j + 1];
+                cur[j + 1] = std::cmp::min(
+                    // deletion
+                    tmp + 1,
+                    std::cmp::min(
+                        // insertion
+                        cur[j] + 1,
+                        // match or substitution
+                        pre + if cx == cy { 0 } else { 1 },
+                    ),
+                );
+                pre = tmp;
+            }
+        }
+        U::from(cur[len_y - 1]).unwrap()
+    }
+}
